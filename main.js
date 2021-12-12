86d7c4935bd037212a2785e86e4e6a82a856475d
Gabriela Mutilva <66925220+gabymy@users.noreply.github.com>
2021-12-12 12:27:30 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+const switchButton = document.getElementById('switch');
+
+switchButton.addEventListener('click', () => {
+    document.body.classList.toggle('dark'); 
+    switchButton.classList.toggle('active');
+});
+
+
+
+$(document).ready(main);
+
+var contador = 1;
+
+function main () {
+	$('.menu_bar').click(function(){
+		if (contador == 1) {
+			$('nav').animate({
+				left: '0'
+			});
+			contador = 0;
+		} else {
+			contador = 1;
+			$('nav').animate({
+				left: '-100%'
+			});
+		}
+	});
+
+	// Mostramos y ocultamos submenus
+	$('.submenu').click(function(){
+		$(this).children('.children').slideToggle();
+	});
+}
